6c73f9c8a7ed988d7d9693f9a83615df59540cf0
Cristian Restrepo <cristianrestrepo13@gmail.com>
2020-05-28 09:13:40 -0500

    Updates

---
+++ precache-manifest.c26e8d505e466671c39326fc92ffbbe8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef9d5c212f60d5e696c9c8a9a163b14",
+    "url": "/plantapp/index.html"
+  },
+  {
+    "revision": "13551d682f425aac2a91",
+    "url": "/plantapp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "567e113cc3028ec21478",
+    "url": "/plantapp/static/css/main.0a54a213.chunk.css"
+  },
+  {
+    "revision": "13551d682f425aac2a91",
+    "url": "/plantapp/static/js/2.e150fbdd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/plantapp/static/js/2.e150fbdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "567e113cc3028ec21478",
+    "url": "/plantapp/static/js/main.cb21a55c.chunk.js"
+  },
+  {
+    "revision": "de90aabc4e4c5f21933f",
+    "url": "/plantapp/static/js/runtime-main.659f039d.js"
+  },
+  {
+    "revision": "ba631b638722bb02c7a4ee1998ba6224",
+    "url": "/plantapp/static/media/banner_img.ba631b63.jpg"
+  },
+  {
+    "revision": "0d3d721254ae1141a1533f413fcd768c",
+    "url": "/plantapp/static/media/logo.0d3d7212.png"
+  }
+]);
